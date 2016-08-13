63a288280cb2d4fd6f88c96f134eff4050c870fe
Derek <derekt1026@gmail.com>
2016-08-12 20:02:02 -0500

    Added second PickerView for dust level

--- IV Calculator/IV Calculator/IVCalculatorViewController.swift
+++ IV Calculator/IV Calculator/IVCalculatorViewController.swift
@@ -16,15 +16,38 @@ class IVCalculatorViewController: UIViewController, UIPickerViewDataSource, UIPi
     @IBOutlet weak var dustTextField: UITextField!
     @IBOutlet weak var poweredTextField: UISwitch!
     
-    var pokemonNamePickerView = UIPickerView()
+    // Tag enum for UIPickers
+    enum PickerViewTag: Int {
+        case PokeNameList
+        case DustPowerLevel
+    }
+    
+    // UIPickers
+    var pokemonNameList = UIPickerView()
+    var dustLevel = UIPickerView()
+    
+    // Lists for UIPickers
     var pokeNames = ["Abra", "Aerodactyl", "Alakazam", "Arbok", "Arcanine", "Beedrill", "Bellsprout", "Blastoise", "Bulbasaur", "Butterfree", "Caterpie", "Chansey", "Charizard", "Charmander", "Charmeleon", "Clefable", "Clefairy", "Cloyster", "Cubone", "Dewgong", "Diglett", "Dodrio", "Doduo", "Dragonair", "Dragonite", "Dratini", "Drowzee", "Dugtrio", "Eevee", "Ekans", "Electabuzz", "Electrode", "Exeggcute", "Exeggutor", "Farfetch'd", "Fearow", "Flareon", "Gastly", "Gengar", "Geodude", "Golbat", "Goldeen", "Golduck", "Golem", "Graveler", "Grimer", "Growlithe", "Gyarados", "Haunter", "Hitmonchan", "Hitmonlee", "Horsea", "Hypno", "Ivysaur", "Jigglypuff", "Jolteon", "Jynx", "Kabuto", "Kabutops", "Kadabra", "Kakuna", "Kangaskhan", "Kingdra", "Kingler", "Koffing", "Krabby", "Lapras", "Lickitung", "Machamp", "Machoke", "Machop", "Magikarp", "Magmar", "Magnemite", "Magneton", "Mankey", "Marowak", "Meowth", "Metapod", "Mr. Mime", "Nidoking", "Nidoqueen", "Nidoran (Female)", "Nidoran (Male)", "Nidorina", "Nidorino", "Ninetales", "Oddish", "Omanyte", "Omastar", "Onix", "Paras", "Parasect", "Persian", "Pidgeot", "Pidgeotto", "Pidgey", "Pikachu", "Pinsir", "Poliwag", "Poliwhirl", "Poliwrath", "Ponyta", "Porygon", "Primeape", "Psyduck", "Raichu", "Rapidash", "Raticate", "Rattata", "Rhydon", "Rhyhorn", "Sandshrew", "Sandslash", "Scyther", "Seadra", "Seaking", "Shellder", "Slowbro", "Slowpoke", "Snorlax", "Squirtle", "Starmie", "Staryu", "Tangela", "Tauros", "Tentacool", "Tentacruel", "Vaporeon", "Venomoth", "Venonat", "Venusaur", "Victreebel", "Vileplume", "Voltorb", "Vulpix", "Wartortle", "Weedle", "Weepinbell", "Weezing", "Wigglytuff", "Zubat"]
+    var dustList = [200, 400, 600, 800, 1000, 1300, 1600, 1900, 2200, 2500, 3000, 3500, 4000, 4500, 5000, 6000, 7000, 8000, 9000, 10000]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set tags for PickerViews
+        pokemonNameList.tag = PickerViewTag.PokeNameList.rawValue
+        dustLevel.tag = PickerViewTag.DustPowerLevel.rawValue
+        
+        // Initalize Pokemon Name text box and picker
         pokemonNameTextField.text = pokeNames[0]
-        pokemonNamePickerView.delegate = self
-        pokemonNamePickerView.dataSource = self
-        pokemonNameTextField.inputView = pokemonNamePickerView
+        pokemonNameList.delegate = self
+        pokemonNameList.dataSource = self
+        pokemonNameTextField.inputView = pokemonNameList
+        
+        // Initalize dust level box and picker
+        dustTextField.text = String(dustList[0])
+        dustLevel.delegate = self
+        dustLevel.dataSource = self
+        dustTextField.inputView = dustLevel
     }
     
     // Returns number of columns to display
@@ -34,15 +57,40 @@ class IVCalculatorViewController: UIViewController, UIPickerViewDataSource, UIPi
     
     // Returns number of rows in component
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if let tag = PickerViewTag(rawValue: pickerView.tag) {
+            switch tag {
+            case .PokeNameList:
+                return pokeNames.count
+            case .DustPowerLevel:
+                return dustList.count
+            }
+        }
+        // Fix?
         return pokeNames.count
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if let tag = PickerViewTag(rawValue: pickerView.tag) {
+            switch tag {
+            case .PokeNameList:
+                return pokeNames[row]
+            case .DustPowerLevel:
+                return String(dustList[row])
+            }
+        }
+        // Fix?
         return pokeNames[row]
     }
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        pokemonNameTextField.text = pokeNames[row]
+        if let tag = PickerViewTag(rawValue: pickerView.tag) {
+            switch tag {
+            case .PokeNameList:
+                pokemonNameTextField.text = pokeNames[row]
+            case .DustPowerLevel:
+                dustTextField.text = String(dustList[row])
+            }
+        }
     }
     
     // TODO: Add buttons for selecting in pickerView
